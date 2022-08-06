aa49de7ee152ce0c59c95866d91e79d06d7e7584
Shi Han <shihanlim128@gmail.com>
2022-08-06 17:35:34 +0800

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(small, big) {
+    if(!(Number.isInteger(small)) || !(Number.isInteger(big)) || small < 0 || big < 0) {
+        return "ERROR";
+    }
+    if (big < small) {
+        const temp = small;
+        small = big;
+        big = temp;
+    }
+    let result = 0;
+    for (let i = small; i <= big; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
