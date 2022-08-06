60eff25d637e5a57569fd13787612eb7a08125d0
Shi Han <shihanlim128@gmail.com>
2022-08-06 16:29:36 +0800

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const wordList = word.split("");
+    const rWordList = wordList.reverse();
+    return wordList.join("")
 };
 
 // Do not edit below this line
