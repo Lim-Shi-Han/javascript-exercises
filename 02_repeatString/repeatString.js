d700712ccfd03a59e6966cf42b38ba9548172401
Shi Han <shihanlim128@gmail.com>
2022-08-06 16:09:06 +0800

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let result = ""
+    for (times; times > 0; times--) {
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line
