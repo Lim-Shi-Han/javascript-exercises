49e85e00bd3432bdf1ea36fd9d2cffb46bc2c9f9
Shi Han <shihanlim128@gmail.com>
2022-08-06 17:14:17 +0800

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
 const removeFromArray = function() {
-
+    const args = Array.from(arguments)
+    let originalArray = args[0];
+    let removeArray = args.slice(1);
+    return originalArray.filter((ele) => !removeArray.includes(ele));
 };
 
 // Do not edit below this line
