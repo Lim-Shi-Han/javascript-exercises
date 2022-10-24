323d4e84801c1d2a5addc33a0b28db9b02295904
Shi Han <shihanlim128@gmail.com>
2022-10-24 12:55:40 +0800

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    // return array.sort((a, b) => (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) ? -1 : 1)[0]
+    // return array.sort(person => person.yearOfDeath - person.yearOfBirth)[array.length - 1] DOESN'T WORK YET
+    return array.reduce((oldest, current) => (getAge(oldest.yearOfBirth, oldest.yearOfDeath) > getAge(current.yearOfBirth, current.yearOfDeath)) ? oldest : current);
 };
+
+const getAge = function(yearOfBirth, yearOfDeath) {
+    if (!yearOfDeath) {
+        yearOfDeath = new Date().getFullYear();
+    }
+    return yearOfDeath - yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
