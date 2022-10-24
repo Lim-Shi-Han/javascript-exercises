bb0ba54da70bcdd5a4ee32713faab4b010798552
Shi Han <shihanlim128@gmail.com>
2022-10-24 12:22:18 +0800

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS"
+    if (x == 1 || x == 2) return 1;
+    smallNum = 1;
+    bigNum = 1;
+    for (let i = 2; i < x; i++) {
+        bigNum = bigNum + smallNum;
+        smallNum = bigNum - smallNum;
+    }
+    return bigNum
 };
 
 // Do not edit below this line
