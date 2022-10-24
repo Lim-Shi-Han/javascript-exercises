b9be3ea3e5417c9a52b48af347c5f860dc62bfc5
Shi Han <shihanlim128@gmail.com>
2022-10-24 12:16:38 +0800

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let newText = text
+                    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+                    .replace(/\s+/g, "")
+                    .toLowerCase();
+    smallIndex = 0;
+    largeIndex = newText.length - 1;
+    while (smallIndex < largeIndex) {
+        if (newText.charAt(smallIndex) !== newText.charAt(largeIndex)){
+            return false;
+        }
+        smallIndex++;
+        largeIndex--;
+    }
+    return true;
 };
 
 // Do not edit below this line
